3446e89a99fab2871692d7cdaa497ef912451791
tokizuoh <tokizuoh@gmail.com>
2022-05-08 10:47:51 +0900

    [#1] サイクリング一覧セルのViewを追加

---
+++ contrail/View/CyclingDistanceRow.swift
@@ -0,0 +1,38 @@
+//
+//  CyclingDistanceRow.swift
+//  contrail
+//
+//  Created by tokizo on 2022/05/08.
+//
+
+import SwiftUI
+
+struct CyclingDistanceRow: View {
+    let distance: Double
+    let date: String
+
+    var body: some View {
+        HStack(alignment: .bottom) {
+            Text("\(distance.description) km")
+                .font(.title)
+                .padding(EdgeInsets(top: 0,
+                                    leading: 20,
+                                    bottom: 0,
+                                    trailing: 0))
+            Spacer()
+            Text(date)
+                .font(.subheadline)
+                .padding(EdgeInsets(top: 0,
+                                    leading: 0,
+                                    bottom: 0,
+                                    trailing: 20))
+        }
+    }
+}
+
+struct CyclingDistanceRow_Previews: PreviewProvider {
+    static var previews: some View {
+        CyclingDistanceRow(distance: 41.456,
+                           date: "2022.05.08")
+    }
+}
