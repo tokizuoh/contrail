cdb3ee9b3c695c171028516faea5f1116495051d
tokizuoh <tokizuoh@gmail.com>
2022-05-08 11:02:03 +0900

    [#1] ListのViewを定義

--- contrail/View/CyclingDistanceListView.swift
+++ contrail/View/CyclingDistanceListView.swift
@@ -1,5 +1,5 @@
 //
-//  CyclingDistanceList.swift
+//  CyclingDistanceListView.swift
 //  contrail
 //
 //  Created by tokizo on 2022/05/08.
@@ -8,15 +8,25 @@
 import SwiftUI
 
 struct CyclingDistanceListView: View {
-    let CyclingDistance
+    let cyclingDistanceList: [CyclingDistance]
     
     var body: some View {
-        List
+        List(cyclingDistanceList) { cyclingDistance in
+            CyclingDistanceRow(distance: cyclingDistance.distance,
+                               date: cyclingDistance.date)
+        }
     }
 }
 
-struct CyclingDistanceList_Previews: PreviewProvider {
+struct CyclingDistanceListView_Previews: PreviewProvider {
     static var previews: some View {
-        CyclingDistanceList()
+        CyclingDistanceListView(cyclingDistanceList: [
+            .init(distance: 41.1, date: "2022.05.08"),
+            .init(distance: 22.4, date: "2022.05.03"),
+            .init(distance: 35.1, date: "2022.04.29"),
+            .init(distance: 33.3, date: "2022.04.24"),
+            .init(distance: 19.2, date: "2022.04.21"),
+            .init(distance: 48.5, date: "2022.04.20"),
+        ])
     }
 }
