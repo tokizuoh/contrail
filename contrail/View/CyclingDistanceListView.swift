8592b6ea9b347d41d8066ae5b01ca77be4541c60
tokizuoh <tokizuoh@gmail.com>
2022-05-08 11:01:25 +0900

    [#1] セルに使用する構造体を定義

---
+++ contrail/View/CyclingDistanceListView.swift
@@ -0,0 +1,22 @@
+//
+//  CyclingDistanceList.swift
+//  contrail
+//
+//  Created by tokizo on 2022/05/08.
+//
+
+import SwiftUI
+
+struct CyclingDistanceListView: View {
+    let CyclingDistance
+    
+    var body: some View {
+        List
+    }
+}
+
+struct CyclingDistanceList_Previews: PreviewProvider {
+    static var previews: some View {
+        CyclingDistanceList()
+    }
+}
