8883ea849695ec667228a6b155aa095b614c93a3
tokizuoh <tokizuoh@gmail.com>
2022-05-14 17:29:20 +0900

    [#39] 削除したクラスの宣言を削除

--- contrail/View/ContentView.swift
+++ contrail/View/ContentView.swift
@@ -9,12 +9,7 @@ import SwiftUI
 import HealthKit
 
 struct ContentView: View {
-    // TODO: [#30] HealthKitClient の命名を考え直す
     @ObservedObject var viewModel = ContentViewModel()
-
-    //    init() {
-    //        healthKitClient = HealthKitClient.shared
-    //    }
 
     var body: some View {
         CyclingDistanceListView(viewModel: viewModel.data)
