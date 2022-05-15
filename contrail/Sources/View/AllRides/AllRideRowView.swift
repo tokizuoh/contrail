bf66f35e7ac299b6d8b9082867523a15f1e48535
tokizuoh <tokizuoh@gmail.com>
2022-05-15 22:44:31 +0900

    [#67] Viewをcomponent化

--- contrail/Sources/View/AllRides/AllRideRowView.swift
+++ contrail/Sources/View/AllRides/AllRideRowView.swift
@@ -12,28 +12,33 @@ struct AllRideRowView: View {
 
     var body: some View {
         VStack {
-            HStack(alignment: .lastTextBaseline) {
-                HStack(alignment: .lastTextBaseline) {
-                    Text(rideDetail.distanceText)
-                        .font(.title)
-                    Text("km")
-                        .font(.subheadline)
-                }
-                .foregroundColor(.contrailBrand)
-                .padding(.vertical, 5)
-
-                Spacer()
-
-                Text(rideDetail.dateText)
-                    .font(.subheadline)
-                    .foregroundColor(.contrailLightGray2)
-                    .padding(.vertical, 5)
-            }
+            distanceComponent
             Divider()
                 .padding(.bottom, 4)
 
             Text(rideDetail.durationText)
             Spacer()
+        }
+    }
+
+    // MARK: - component
+    var distanceComponent: some View {
+        HStack(alignment: .lastTextBaseline) {
+            HStack(alignment: .lastTextBaseline) {
+                Text(rideDetail.distanceText)
+                    .font(.title)
+                Text("km")
+                    .font(.subheadline)
+            }
+            .foregroundColor(.contrailBrand)
+            .padding(.vertical, 5)
+
+            Spacer()
+
+            Text(rideDetail.dateText)
+                .font(.subheadline)
+                .foregroundColor(.contrailLightGray2)
+                .padding(.vertical, 5)
         }
     }
 }
