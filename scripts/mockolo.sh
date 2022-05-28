15fe0e274465c3d83948fd2e52d7890aab805b47
tokizuoh <tokizuoh@gmail.com>
2022-05-28 14:30:42 +0900

    [#9] 不要な改行を削除

--- scripts/mockolo.sh
+++ scripts/mockolo.sh
@@ -1,5 +1,4 @@
 #!/bin/sh
-
 if which mint >/dev/null; then
     rm -f $SRCROOT/contrail/MockResults.swift
     mint run mockolo mockolo --sourcedirs $SRCROOT/contrail --destination $SRCROOT/contrail/MockResults.swift --mock-final
