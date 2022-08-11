f9a7862f5f149593170926cb5768eb6b0d4fb879
tokizuoh <tokizuoh@gmail.com>
2022-08-11 09:21:08 +0900

    [#125] XcodeプロジェクトをUIKitベースで新規作成

---
+++ contrail/contrail/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  contrail
+//
+//  Created by tokizo on 2022/08/11.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
