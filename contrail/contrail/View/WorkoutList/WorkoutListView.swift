8bf9046d8e9027fbfd564ed7b0fc8fa4472205e6
tokizuoh <tokizuoh@gmail.com>
2022-08-14 15:35:33 +0900

    [#139] ViewModelを新規ファイルに切り出した

--- contrail/contrail/View/WorkoutList/WorkoutListView.swift
+++ contrail/contrail/View/WorkoutList/WorkoutListView.swift
@@ -30,31 +30,6 @@ struct WorkoutListView_Previews: PreviewProvider {
     }
 }
 
-struct WorkoutListData {
-    let workoutCellItems: [TopWorkoutCellItem]
-
-    static func empty() -> Self {
-        return .init(workoutCellItems: [])
-    }
-}
-
-final class WorkoutListVideModel: ObservableObject {
-    @Published var data: WorkoutListData = .empty()
-    private let workoutsCacher: WorkoutsCacherProtocol
-
-    init(workoutsCacher: WorkoutsCacherProtocol) {
-        self.workoutsCacher = workoutsCacher
-    }
-
-    func dispatch() {
-        guard let workouts = workoutsCacher.getWorkouts() else {
-            return
-        }
-        let workoutCellItems = WorkoutCellTranslator.translate(workouts)
-        data = WorkoutListData(workoutCellItems: workoutCellItems)
-    }
-}
-
 struct WorkoutListScreenBuilder {
     static func build() -> UIHostingController<WorkoutListView> {
         let vc = UIHostingController(rootView: WorkoutListView())
