feb822f378846b4ba3eb8bd4b95e9c8ebd04742a
tokizuoh <tokizuoh@gmail.com>
2022-09-04 12:15:55 +0900

    translatorを追加

--- contrail/contrail/Presentation/AnalyticsDetail/Yearly/AnalyticsDetailYearlyViewModel.swift
+++ contrail/contrail/Presentation/AnalyticsDetail/Yearly/AnalyticsDetailYearlyViewModel.swift
@@ -32,6 +32,68 @@ final class AnalyticsDetailYearlyViewModel: ObservableObject {
         guard let workouts = workoutsCacher.getWorkouts() else {
             return
         }
-        //        data = AnalyticsDetailTranslator.translate(workouts, option: .monthly)
+        data = AnalyticsDetailYearlyTranslator.translate(workouts, option: .yearly)
+        print(data.chartViewItem.workoutItems)
     }
+}
+
+struct AnalyticsDetailYearlyTranslator {
+    typealias From = [HKWorkout]
+    typealias To = AnalyticsDetailYearlyData
+
+    enum Option {
+        case yearly
+    }
+
+    static func translate(_ from: From, option: Option) -> To {
+        let now = Date()
+        var calendar = Calendar(identifier: .japanese)
+        let timeZone = TimeZone(identifier: "Asia/Tokyo")!
+        calendar.timeZone = timeZone
+        calendar.locale = Locale(identifier: "ja_JP")
+
+        let toGranularity: Calendar.Component = {
+            switch option {
+            case .yearly:
+                return .year
+            }
+        }()
+
+        var totalDistance: Double = 0.0
+        let workoutItems: [AnalyticsDetailYearlyWorkoutItem] = from.compactMap { workout in
+            guard let distance = workout.totalDistance?.kilometers(),
+                  calendar.isDate(now, equalTo: workout.startDate, toGranularity: toGranularity) else {
+                return nil
+            }
+            guard let workoutType: AnalyticsDetailYearlyWorkoutItem.WorkoutType = {
+                switch workout.workoutActivityType {
+                case .cycling:
+                    return .cycling
+                case .running:
+                    return .running
+                default:
+                    return nil
+                }
+            }() else {
+                return nil
+            }
+            totalDistance += distance
+
+            let dateComponents = calendar.dateComponents([.timeZone, .calendar, .year, .month], from: workout.startDate)
+            return .init(
+                type: workoutType,
+                date: dateComponents.date!,
+                distance: distance
+            )
+        }
+        let totalDistanceString = String(format: "%.2f", totalDistance)
+        return .init(
+            chartViewItem:
+                .init(
+                    totalDistanceString: totalDistanceString,
+                    workoutItems: workoutItems
+                )
+        )
+    }
+
 }
