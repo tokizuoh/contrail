e7a3d6351c8f729e33e512a0311ab82cf7506b9a
tokizuoh <tokizuoh@gmail.com>
2022-12-17 19:16:06 +0900

    chartViewをpropertyに切り出した

--- contrail/contrail/Presentation/Abstract/AbstractChartView.swift
+++ contrail/contrail/Presentation/Abstract/AbstractChartView.swift
@@ -32,41 +32,7 @@ struct AbstractChartView: View {
     var body: some View {
         ZStack(alignment: .topTrailing) {
             shareButton
-            VStack(alignment: .leading) {
-                VStack(
-                    alignment: .leading,
-                    spacing: 2
-                ) {
-                    Text("Total Distances")
-                        .font(.subheadline)
-                        .foregroundColor(.lightGray)
-                    Text("\(data.totalDistanceString) KM")
-                        .font(.title2)
-                        .bold()
-                }
-                Chart {
-                    ForEach(data.workoutItems) { item in
-                        BarMark(
-                            x: .value("Shape Type", item.date.formatted(.dateTime.month(.twoDigits))),
-                            y: .value("Distance", item.distance)
-                        )
-                        .foregroundStyle(by: .value("Distance", item.type.rawValue))
-                    }
-                }
-                .chartXAxis {
-                    AxisMarks(position: .bottom) { _ in
-                        AxisGridLine(centered: true)
-                        AxisTick(centered: true)
-                        AxisValueLabel()
-                    }
-                }
-                .chartForegroundStyleScale([
-                    AbstractChartViewWorkoutItem.WorkoutType.cycling.rawValue: .blue,
-                    AbstractChartViewWorkoutItem.WorkoutType.running.rawValue: .green,
-                    AbstractChartViewWorkoutItem.WorkoutType.walking.rawValue: .yellow
-                ])
-                .frame(height: 250)
-            }
+            chartView
         }
         .padding(20)
         .background(Color.darkGray)
@@ -85,6 +51,44 @@ struct AbstractChartView: View {
                 applicationActivities: nil
             )
             .presentationDetents([.medium])
+        }
+    }
+
+    private var chartView: some View {
+        VStack(alignment: .leading) {
+            VStack(
+                alignment: .leading,
+                spacing: 2
+            ) {
+                Text("Total Distances")
+                    .font(.subheadline)
+                    .foregroundColor(.lightGray)
+                Text("\(data.totalDistanceString) KM")
+                    .font(.title2)
+                    .bold()
+            }
+            Chart {
+                ForEach(data.workoutItems) { item in
+                    BarMark(
+                        x: .value("Shape Type", item.date.formatted(.dateTime.month(.twoDigits))),
+                        y: .value("Distance", item.distance)
+                    )
+                    .foregroundStyle(by: .value("Distance", item.type.rawValue))
+                }
+            }
+            .chartXAxis {
+                AxisMarks(position: .bottom) { _ in
+                    AxisGridLine(centered: true)
+                    AxisTick(centered: true)
+                    AxisValueLabel()
+                }
+            }
+            .chartForegroundStyleScale([
+                AbstractChartViewWorkoutItem.WorkoutType.cycling.rawValue: .blue,
+                AbstractChartViewWorkoutItem.WorkoutType.running.rawValue: .green,
+                AbstractChartViewWorkoutItem.WorkoutType.walking.rawValue: .yellow
+            ])
+            .frame(height: 250)
         }
     }
 }
