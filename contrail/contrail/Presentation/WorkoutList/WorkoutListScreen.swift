feb1e90ab01fe3c18c23226bb366c2bfd9fb3119
tokizuoh <tokizuoh@gmail.com>
2022-12-18 14:29:02 +0900

    WorkoutListScreenの仮実装を追加

---
+++ contrail/contrail/Presentation/WorkoutList/WorkoutListScreen.swift
@@ -0,0 +1,33 @@
+//
+//  WorkoutListScreen.swift
+//  contrail
+//
+//  Created by tokizo on 2022/12/18.
+//
+
+import SwiftUI
+
+struct WorkoutListScreen: View {
+    @ObservedObject var viewModel: WorkoutListScreenViewModel
+
+    var body: some View {
+        ScrollView {
+            LazyVStack(
+                alignment: .leading,
+                spacing: 10
+            ) {
+                ForEach(viewModel.data.sectionList) { section in
+                    ForEach(section.itemList) { item in
+                        WorkoutItemView(item: item)
+                    }
+                }
+            }
+            .padding(.vertical, 20)
+            .padding(.horizontal, 16)
+        }
+        .navigationTitle("Workouts")
+        .onAppear {
+            viewModel.dispatch()
+        }
+    }
+}
