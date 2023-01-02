fc4ce08e857549a0635c2c60dd16d4ff82bcd3df
tokizuoh <tokizuoh@gmail.com>
2023-01-02 15:24:36 +0900

    ChartViewの追加

---
+++ contrail/contrail/Presentation/PeriodChart/ChartView.swift
@@ -0,0 +1,73 @@
+//
+//  ChartView.swift
+//  contrail
+//
+//  Created by tokizo on 2023/01/02.
+//
+
+import Charts
+import SwiftUI
+
+struct ChartItem: Identifiable {
+    let date: Date
+    let quantity: Double
+    var id: Date { date }
+}
+
+struct ChartView: View {
+    let items: [ChartItem]
+
+    var body: some View {
+        Chart(items) { item in
+            BarMark(
+                x: .value("Day", item.date, unit: .day),
+                y: .value("Quantity", item.quantity)
+            )
+        }
+        .chartXAxis {
+            AxisMarks(values: .stride(by: .day)) { _ in
+                AxisGridLine()
+                AxisValueLabel(format: .dateTime.weekday())
+            }
+        }
+    }
+}
+
+#if DEBUG
+struct ChartView_Previews: PreviewProvider {
+    static private var chartItems: [ChartItem] = [
+        .init(
+            date: Calendar.current.date(byAdding: .day, value: -6, to: Date())!,
+            quantity: 30.0
+        ),
+        .init(
+            date: Calendar.current.date(byAdding: .day, value: -5, to: Date())!,
+            quantity: 120.0
+        ),
+        .init(
+            date: Calendar.current.date(byAdding: .day, value: -4, to: Date())!,
+            quantity: 100.0
+        ),
+        .init(
+            date: Calendar.current.date(byAdding: .day, value: -3, to: Date())!,
+            quantity: 10.0
+        ),
+        .init(
+            date: Calendar.current.date(byAdding: .day, value: -2, to: Date())!,
+            quantity: 176.0
+        ),
+        .init(
+            date: Calendar.current.date(byAdding: .day, value: -1, to: Date())!,
+            quantity: 100.0
+        ),
+        .init(
+            date: Date(),
+            quantity: 40.0
+        )
+    ]
+
+    static var previews: some View {
+        ChartView(items: chartItems)
+    }
+}
+#endif
--- contrail/contrail/Presentation/PeriodChart/PeriodChartScreen.swift
+++ contrail/contrail/Presentation/PeriodChart/PeriodChartScreen.swift
@@ -5,12 +5,29 @@
 //  Created by tokizo on 2023/01/02.
 //
 
+import Charts
 import SwiftUI
 
 struct PeriodChartScreen: View {
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
-            .navigationTitle("Active Energy")
-            .navigationBarTitleDisplayMode(.inline)
+        VStack(alignment: .leading) {
+            Text("TODO")
+            Text("TODO")
+            ChartView(items: [])
+            .padding(.vertical, 20)
+            .frame(height: 350)
+        }
+        .padding(.horizontal, 24)
+        .navigationTitle("Active Energy")
+        .navigationBarTitleDisplayMode(.inline)
+        .background(Color.darkGray)
     }
 }
+
+#if DEBUG
+struct PeriodChartScreen_Previews: PreviewProvider {
+    static var previews: some View {
+        PeriodChartScreen()
+    }
+}
+#endif
