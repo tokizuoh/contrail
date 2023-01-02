91ba262216ede00a8241e1f13a7dfa21885ef981
tokizuoh <tokizuoh@gmail.com>
2023-01-02 16:20:57 +0900

    ScrollViewを追加

--- contrail/contrail/Presentation/PeriodChart/PeriodChartScreen.swift
+++ contrail/contrail/Presentation/PeriodChart/PeriodChartScreen.swift
@@ -13,14 +13,16 @@ struct PeriodChartScreen: View {
     var viewModel = PeriodChartScreenViewModel(workoutsCacher: WorkoutsCacher.shared)
 
     var body: some View {
-        VStack(alignment: .leading) {
-            Text("TODO")
-            Text("TODO")
-            ChartView(items: viewModel.data.chartItems)
-            .padding(.vertical, 20)
-            .frame(height: 350)
+        ScrollView {
+            VStack(alignment: .leading) {
+                Text("TODO")
+                Text("TODO")
+                ChartView(items: viewModel.data.chartItems)
+                    .padding(.vertical, 20)
+                    .frame(height: 350)
+            }
+            .padding(.horizontal, 24)
         }
-        .padding(.horizontal, 24)
         .navigationTitle("Active Energy")
         .navigationBarTitleDisplayMode(.inline)
         .background(Color.darkGray)
