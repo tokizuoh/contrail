6094ebe1dbee7e3fc767a24e438d203ffd1efdc9
tokizuoh <tokizuoh@gmail.com>
2023-01-02 11:45:17 +0900

    navigationTitleの設定を追加

--- contrail/contrail/Presentation/PeriodChart/PeriodChartScreen.swift
+++ contrail/contrail/Presentation/PeriodChart/PeriodChartScreen.swift
@@ -10,5 +10,7 @@ import SwiftUI
 struct PeriodChartScreen: View {
     var body: some View {
         Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+            .navigationTitle("Active Energy")
+            .navigationBarTitleDisplayMode(.inline)
     }
 }
