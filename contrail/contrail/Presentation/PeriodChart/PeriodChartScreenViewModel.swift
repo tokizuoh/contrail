3be4194e4efedb6f53a4117b226a0f7719955d52
tokizuoh <tokizuoh@gmail.com>
2023-01-03 11:05:36 +0900

    Translatorを新規ファイルに切り出した

--- contrail/contrail/Presentation/PeriodChart/PeriodChartScreenViewModel.swift
+++ contrail/contrail/Presentation/PeriodChart/PeriodChartScreenViewModel.swift
@@ -33,26 +33,3 @@ final class PeriodChartScreenViewModel: ObservableObject {
         print(data)
     }
 }
-
-struct PeriodChartDataTranslator: Translator {
-    typealias From = [HKWorkout]
-    typealias To = PeriodChartData
-
-    static func translate(_ from: From) -> To {
-        let dateBeforeAWeek = Calendar.current.date(byAdding: .day, value: -7, to: Date())!
-        var total: Double = 0
-        let chartItems: [ChartItem] = from.compactMap { workout in
-            guard workout.startDate > dateBeforeAWeek,
-                  let statistics = workout.statistics(for: HKQuantityType(.activeEnergyBurned)),
-                  let kilocalorie = statistics.sumQuantity()?.doubleValue(for: .kilocalorie()) else {
-                return nil
-            }
-            total += kilocalorie
-            return ChartItem(
-                date: workout.startDate,
-                quantity: kilocalorie
-            )
-        }
-        return To(chartItem: (total: total, items: chartItems))
-    }
-}
